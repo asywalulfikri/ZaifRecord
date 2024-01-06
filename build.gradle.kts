c979f18df0c82b9fd4434ce538e98cd0b3d24f76
asywalulfikri <asywalul.fikri@ottodigital.id>
2024-01-06 13:01:54 +0700

    yyy

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,12 @@
 buildscript {
     dependencies {
         classpath ("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+
     }
 }
 plugins {
     id("com.android.application") version "8.2.0" apply false
+    id ("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
